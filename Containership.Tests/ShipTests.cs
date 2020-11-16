49de533bf6bbfcbb22eaaeb27625048de8011d7d
Piet2001 <piet2001@fireman.net>
2020-11-16 14:15:27 +0100

    Create Test project

---
+++ Containership.Tests/ShipTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Containership.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
